fea6442dd933d1a6231bec48b69093fe5a36da80
Lucas Gradilone <lucasgradilone@gmail.com>
2023-11-23 01:13:31 -0300

    link do javascript

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const fname = document.getElementById('email');
 const lname = document.getElementById('nome');
-const submit = document.getElementById('envia');
+const submit = document.getElementsByClassName("form-contact")[0];
 
 submit.addEventListener('submit',(e)=>{
     e.preventDefault();
-    let ebody = `
-    <h1>First Name: </h1>${fname.value}
-    <br>
-    <h1>Last Name: </h1>${lname.value}
-    `;
+    console.log("Clicou");
+
+    Email.send({
+        SecureToken : "b4ec64d9-7642-42fa-911d-d03a1db57dce",
+        To : 'lucas.valias@fatec.sp.gov.br',
+        From : "lucas.valias@fatec.sp.gov.br",
+        Subject : "Teste",
+        Body : "Meu primeiro teste"
+    }).then(
+      message => alert(message)
+    );
 });
